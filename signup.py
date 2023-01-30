83cc9b396eacf4371f970e44d51bc7f47b80c5f4
Naufal Zaid <naufaljpnmy@gmail.com>
2023-01-31 02:08:01 +0800

    sign up update

--- signup.py
+++ signup.py
@@ -1,9 +1,31 @@
 print("Register Account")
 name = input("Enter your name: ")
 email = input("Enter your email: ")
-password = input("Enter your password: ")
 
-# Write the data in users.txt
-f = open("accounts.txt", "a")
+# Check if the email is already registered
+f = open("resources/users.txt", "r")
+Lines = f.readlines()
+emailInFile = True
+while emailInFile == True:
+    for line in Lines:
+        if email in line:
+            print("Email already registered")
+        else:
+            emailInFile = False
+f.close()
+
+
+passwordAccepted = False
+while not passwordAccepted:
+    password = input("Enter your password: ")
+    if len(password) < 8:
+        print("Password must be 8 characters long")
+    else:
+        passwordAccepted = True
+        print("Account created successfully")
+
+
+# Write the data in resources/users.txt
+f = open("resources/users.txt", "a")
 f.write(email+","+password+","+name+"\n")
 f.close()
